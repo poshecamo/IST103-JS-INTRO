d8f9d1ce22315970d6cec434bb92a3d61e779858
poshecamo <poshecamo@gmail.com>
2024-04-11 23:21:30 -0700

    Added 'to do' list array

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-
+//activity 1
 alert("Hello World");
+// alert("Hello World");
+
+
+
+//Activity 2
+let personName ="Damiano";
+let personAge=21;
+const hairColor="Brown";
+let isSingle= True;
+
+console.log(personName+"has" + hairColor + "hair");
+if(isSingle) {
+    console.log("Can Date")
+
+}
+else{
+    console.log("Date Me!")
+}
+
+//Activity 3
+let todolist= ["sign the doc","find a key", "buy a pen", "travel to Japan"];
+let myMoisturizer = {
+    brand:"Laneige",
+    kind:"Treatment",
+    type: "Healing",
+    color:"Blue",
+    issued: true,
+    numberUsed: 34
+
+
+};
+console.log("first thing to accomplish is:"+todolist[0]);
+console.log("mymoisturizer is:"+myMoisturizer.brand);
+
